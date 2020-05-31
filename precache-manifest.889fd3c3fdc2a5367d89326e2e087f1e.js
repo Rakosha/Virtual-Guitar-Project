3a15c7647debe30628f61496e5687d1c70a4118d
Rakosha <rakot1997@gmail.com>
2020-05-31 15:38:56 -0700

    Updates

---
+++ precache-manifest.889fd3c3fdc2a5367d89326e2e087f1e.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa4b3d99b2027b69dcfc14f0f421ff0",
+    "url": "/Virtual-Guitar-Project/index.html"
+  },
+  {
+    "revision": "79d4da6fa750ace9c04d",
+    "url": "/Virtual-Guitar-Project/static/css/main.b78b92d8.chunk.css"
+  },
+  {
+    "revision": "08eb01a43faca4aa5a57",
+    "url": "/Virtual-Guitar-Project/static/js/2.ab5f70fe.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/Virtual-Guitar-Project/static/js/2.ab5f70fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d4da6fa750ace9c04d",
+    "url": "/Virtual-Guitar-Project/static/js/main.988d0c40.chunk.js"
+  },
+  {
+    "revision": "5066cd19fa0bf8333010",
+    "url": "/Virtual-Guitar-Project/static/js/runtime-main.74567882.js"
+  },
+  {
+    "revision": "1c0dd8e1a698e51fd299749ff3698e67",
+    "url": "/Virtual-Guitar-Project/static/media/a1.1c0dd8e1.mp3"
+  },
+  {
+    "revision": "98fe742ca62d27d452176f273b1448ab",
+    "url": "/Virtual-Guitar-Project/static/media/a2.98fe742c.mp3"
+  },
+  {
+    "revision": "d129b246297ad166d89cfe9871822409",
+    "url": "/Virtual-Guitar-Project/static/media/a3.d129b246.mp3"
+  },
+  {
+    "revision": "68d222838f739669601209b469fec271",
+    "url": "/Virtual-Guitar-Project/static/media/a4.68d22283.mp3"
+  },
+  {
+    "revision": "cf217063da6d30128f80bc9bbd9c5a52",
+    "url": "/Virtual-Guitar-Project/static/media/a5.cf217063.mp3"
+  },
+  {
+    "revision": "92711b33fba2e82cd9a2b2e15191e971",
+    "url": "/Virtual-Guitar-Project/static/media/asharp1.92711b33.mp3"
+  },
+  {
+    "revision": "c6048475269f58ca7de59696a6090b43",
+    "url": "/Virtual-Guitar-Project/static/media/asharp2.c6048475.mp3"
+  },
+  {
+    "revision": "19a948ab44ad3fffec51481f3320b479",
+    "url": "/Virtual-Guitar-Project/static/media/asharp3.19a948ab.mp3"
+  },
+  {
+    "revision": "704673c9c2a3fff3336d775cd62decbe",
+    "url": "/Virtual-Guitar-Project/static/media/asharp4.704673c9.mp3"
+  },
+  {
+    "revision": "bb3f140d0f97f3feb47aca86170c966a",
+    "url": "/Virtual-Guitar-Project/static/media/asharp5.bb3f140d.mp3"
+  },
+  {
+    "revision": "0ef238f10aa43b24caabd3b197290d6c",
+    "url": "/Virtual-Guitar-Project/static/media/b1.0ef238f1.mp3"
+  },
+  {
+    "revision": "3f8dc87748edcb18118cd1d3b1e3db51",
+    "url": "/Virtual-Guitar-Project/static/media/b2.3f8dc877.mp3"
+  },
+  {
+    "revision": "4d42b77307188d89a44b4e4c63789ba5",
+    "url": "/Virtual-Guitar-Project/static/media/b3.4d42b773.mp3"
+  },
+  {
+    "revision": "8a1240e906cd17e65d7390c2e399ab61",
+    "url": "/Virtual-Guitar-Project/static/media/b4.8a1240e9.mp3"
+  },
+  {
+    "revision": "ed32541939ce0085c864653df8c24306",
+    "url": "/Virtual-Guitar-Project/static/media/b5.ed325419.mp3"
+  },
+  {
+    "revision": "7e675f5b65ee7c82107914cc0ae50dc9",
+    "url": "/Virtual-Guitar-Project/static/media/bebasneue-regular-webfont.7e675f5b.woff"
+  },
+  {
+    "revision": "f69b3e8cf4d3ca6b5a44afd079964927",
+    "url": "/Virtual-Guitar-Project/static/media/bebasneue-regular-webfont.f69b3e8c.woff2"
+  },
+  {
+    "revision": "a94cd0e484a43192824ed2d314ce1609",
+    "url": "/Virtual-Guitar-Project/static/media/c1.a94cd0e4.mp3"
+  },
+  {
+    "revision": "70d1e34e26cb357627ae6030cf7d434c",
+    "url": "/Virtual-Guitar-Project/static/media/c2.70d1e34e.mp3"
+  },
+  {
+    "revision": "0113d3ee793003abe419fba85434b55f",
+    "url": "/Virtual-Guitar-Project/static/media/c3.0113d3ee.mp3"
+  },
+  {
+    "revision": "47db5234fe34ef12414aa60963a77564",
+    "url": "/Virtual-Guitar-Project/static/media/c4.47db5234.mp3"
+  },
+  {
+    "revision": "ad695bfc3f70d7c2627f56a405ccf964",
+    "url": "/Virtual-Guitar-Project/static/media/c5.ad695bfc.mp3"
+  },
+  {
+    "revision": "9bc373b3f9fb8293575fe2940b4adb11",
+    "url": "/Virtual-Guitar-Project/static/media/c6.9bc373b3.mp3"
+  },
+  {
+    "revision": "8fd43aaf1a5b1ea45e5cb17e850a8498",
+    "url": "/Virtual-Guitar-Project/static/media/csharp1.8fd43aaf.mp3"
+  },
+  {
+    "revision": "9805d908dbb9844c2c531fa1cd957ee8",
+    "url": "/Virtual-Guitar-Project/static/media/csharp2.9805d908.mp3"
+  },
+  {
+    "revision": "f5e297463f17d66c5eb845010db68ce6",
+    "url": "/Virtual-Guitar-Project/static/media/csharp3.f5e29746.mp3"
+  },
+  {
+    "revision": "f98a3acdf49ec66c6ac285b355780963",
+    "url": "/Virtual-Guitar-Project/static/media/csharp4.f98a3acd.mp3"
+  },
+  {
+    "revision": "0fffdae544b0adebae85bd80bcc59353",
+    "url": "/Virtual-Guitar-Project/static/media/csharp5.0fffdae5.mp3"
+  },
+  {
+    "revision": "09aa51b33a9b48844ecbd25d594ee532",
+    "url": "/Virtual-Guitar-Project/static/media/csharp6.09aa51b3.mp3"
+  },
+  {
+    "revision": "fdd2583fc39d98bb7f7cb9392ee29f07",
+    "url": "/Virtual-Guitar-Project/static/media/d1.fdd2583f.mp3"
+  },
+  {
+    "revision": "6e8673aea0680df05f139e6467cfa971",
+    "url": "/Virtual-Guitar-Project/static/media/d2.6e8673ae.mp3"
+  },
+  {
+    "revision": "775b9a850e545328864104cf858f6548",
+    "url": "/Virtual-Guitar-Project/static/media/d3.775b9a85.mp3"
+  },
+  {
+    "revision": "abaa05b194007817f869aecae6bc8884",
+    "url": "/Virtual-Guitar-Project/static/media/d4.abaa05b1.mp3"
+  },
+  {
+    "revision": "92833fae97b95fad36f7029a28cad35c",
+    "url": "/Virtual-Guitar-Project/static/media/d5.92833fae.mp3"
+  },
+  {
+    "revision": "5e5d2d1c870b06e188bdbe07c58947e7",
+    "url": "/Virtual-Guitar-Project/static/media/d6.5e5d2d1c.mp3"
+  },
+  {
+    "revision": "3829077e3e84bd21a1a3810e6634a2b9",
+    "url": "/Virtual-Guitar-Project/static/media/dsharp1.3829077e.mp3"
+  },
+  {
+    "revision": "af69fddeacaa499892af6d1e6946baa0",
+    "url": "/Virtual-Guitar-Project/static/media/dsharp2.af69fdde.mp3"
+  },
+  {
+    "revision": "13d85e6603b0b736d0e538fef95e6458",
+    "url": "/Virtual-Guitar-Project/static/media/dsharp3.13d85e66.mp3"
+  },
+  {
+    "revision": "63a1971a99191df66ce59f7b73a43f5e",
+    "url": "/Virtual-Guitar-Project/static/media/dsharp4.63a1971a.mp3"
+  },
+  {
+    "revision": "68984e26b7b1b4610d5245841d98d3d5",
+    "url": "/Virtual-Guitar-Project/static/media/dsharp5.68984e26.mp3"
+  },
+  {
+    "revision": "22241ae1e8b31437f05475660b11202d",
+    "url": "/Virtual-Guitar-Project/static/media/dsharp6.22241ae1.mp3"
+  },
+  {
+    "revision": "67045af512b034b9df100aa59a8ab83d",
+    "url": "/Virtual-Guitar-Project/static/media/e1.67045af5.mp3"
+  },
+  {
+    "revision": "9ea066747930d4a8561b6c4cbc30561b",
+    "url": "/Virtual-Guitar-Project/static/media/e2.9ea06674.mp3"
+  },
+  {
+    "revision": "9c8af2298bfd64bd4533622b1ffe90fc",
+    "url": "/Virtual-Guitar-Project/static/media/e3.9c8af229.mp3"
+  },
+  {
+    "revision": "6f982b569deed9c46adc78ece0dee039",
+    "url": "/Virtual-Guitar-Project/static/media/e4.6f982b56.mp3"
+  },
+  {
+    "revision": "d58e46eded573520af38606812df8c7a",
+    "url": "/Virtual-Guitar-Project/static/media/e5.d58e46ed.mp3"
+  },
+  {
+    "revision": "fd568981215f5a52a572e64030d9bc86",
+    "url": "/Virtual-Guitar-Project/static/media/e6.fd568981.mp3"
+  },
+  {
+    "revision": "72291b724088f6fa36874b6bd53e287a",
+    "url": "/Virtual-Guitar-Project/static/media/f1.72291b72.mp3"
+  },
+  {
+    "revision": "8889b407cbb7891055825d5384076f44",
+    "url": "/Virtual-Guitar-Project/static/media/f2.8889b407.mp3"
+  },
+  {
+    "revision": "aa74c13a0a11d83ffcd11e837603078d",
+    "url": "/Virtual-Guitar-Project/static/media/f3.aa74c13a.mp3"
+  },
+  {
+    "revision": "5660ddc24a547ddb35b81defc751587a",
+    "url": "/Virtual-Guitar-Project/static/media/f4.5660ddc2.mp3"
+  },
+  {
+    "revision": "3d1edc494e06400dd371076141d51977",
+    "url": "/Virtual-Guitar-Project/static/media/f5.3d1edc49.mp3"
+  },
+  {
+    "revision": "f50fe38b47333b0ce9949d32b4b57e75",
+    "url": "/Virtual-Guitar-Project/static/media/fsharp1.f50fe38b.mp3"
+  },
+  {
+    "revision": "baed4224c1925335999cab9a3e0c5d1f",
+    "url": "/Virtual-Guitar-Project/static/media/fsharp2.baed4224.mp3"
+  },
+  {
+    "revision": "c60624f4cbb054aced7d832be02606d7",
+    "url": "/Virtual-Guitar-Project/static/media/fsharp3.c60624f4.mp3"
+  },
+  {
+    "revision": "427fa58c826c4ad92b8b1f98fc70c63b",
+    "url": "/Virtual-Guitar-Project/static/media/fsharp4.427fa58c.mp3"
+  },
+  {
+    "revision": "c57bbdf0edda1e94876835fd62c0171c",
+    "url": "/Virtual-Guitar-Project/static/media/fsharp5.c57bbdf0.mp3"
+  },
+  {
+    "revision": "9d7ec424916a7175a7df44601f4498fb",
+    "url": "/Virtual-Guitar-Project/static/media/g1.9d7ec424.mp3"
+  },
+  {
+    "revision": "9729880736ab1a7d69d42e6c19295ecc",
+    "url": "/Virtual-Guitar-Project/static/media/g2.97298807.mp3"
+  },
+  {
+    "revision": "e852681606babfc98d89e98441d20d56",
+    "url": "/Virtual-Guitar-Project/static/media/g3.e8526816.mp3"
+  },
+  {
+    "revision": "bbddce366b3a15ef8a93cbfe47c33862",
+    "url": "/Virtual-Guitar-Project/static/media/g4.bbddce36.mp3"
+  },
+  {
+    "revision": "9d306f20404c59e6c45960332328657d",
+    "url": "/Virtual-Guitar-Project/static/media/g5.9d306f20.mp3"
+  },
+  {
+    "revision": "e0390b466d9c35ed90a81a80b9f6539a",
+    "url": "/Virtual-Guitar-Project/static/media/gsharp1.e0390b46.mp3"
+  },
+  {
+    "revision": "b7dd1882828c49cc6ab5eabaa2b487b5",
+    "url": "/Virtual-Guitar-Project/static/media/gsharp2.b7dd1882.mp3"
+  },
+  {
+    "revision": "af14a36f0c487e8e74c0bbfacb921c76",
+    "url": "/Virtual-Guitar-Project/static/media/gsharp3.af14a36f.mp3"
+  },
+  {
+    "revision": "96d4c7ceee3fa1b21309c15c3efb0dc8",
+    "url": "/Virtual-Guitar-Project/static/media/gsharp4.96d4c7ce.mp3"
+  },
+  {
+    "revision": "bc52208c0f348e596d4069c6deecdfe2",
+    "url": "/Virtual-Guitar-Project/static/media/gsharp5.bc52208c.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Virtual-Guitar-Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
